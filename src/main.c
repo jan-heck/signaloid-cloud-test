f779f09a40e630327a674f60f7cd1a34ba96d72c
Jan Heck <43969669+jan-heck@users.noreply.github.com>
2021-12-17 20:22:06 +0100

    Start writing second example

--- src/main.c
+++ src/main.c
@@ -7,19 +7,23 @@ main(int argc, char *  argv[])
 	double a1, a2;
 	const double rangeStart = 0;
 	const double rangeStop = 1;
+	const unsigned long iterations = 5;
 
 	a1 = libUncertainDoubleUniformDist(rangeStart, rangeStop);
 	printf("a1 = %lf\n", a1);
 	libUncertainDoublePrint(a1);
 
-	/*a2 = libUncertainDoubleUniformDist(rangeStart, rangeStop);*/
-	a2 = a1;
-	printf("a2 = %lf\n", a2);
-	libUncertainDoublePrint(a2);
+	for (unsigned long i = 0; i < iterations; i++){
+		printf("i = %d", i);
 
-	double result = a1 - a2;
-	printf("result = %lf\n", result);
-	libUncertainDoublePrint(result);
+		a2 = libUncertainDoubleUniformDist(rangeStart, rangeStop);
+		printf("a2 = %lf\n", a2);
+		libUncertainDoublePrint(a2);
+
+		double result = a1 - a2;
+		printf("result = %lf\n", result);
+		libUncertainDoublePrint(result);
+	}
 
 	return 0;
 }
